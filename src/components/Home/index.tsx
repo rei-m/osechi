7adc9b0046f054770881f6b49c80ebad6a5dcde1
rei-m <star.oceans.rm@gmail.com>
2018-09-07 01:32:08 +0900

    WIP

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,7 +1,24 @@
 import * as React from 'react';
 
 const Home: React.SFC<{}> = ({}) => {
-  return <div>unko</div>;
+  return (
+    <div>
+      <div
+        style={{
+          width: '100%',
+          height: 80,
+          padding: 16,
+          backgroundColor: '#00a381'
+        }}
+      >
+        バナー
+      </div>
+      <div>ソート</div>
+      <div>和おせちリスト</div>
+      <div>洋おせちリスト</div>
+      <div>中おせちリスト</div>
+    </div>
+  );
 };
 
 export default Home;
